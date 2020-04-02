c6ed1e027b9bf27df8a32b23b631f75011833a67
Patryk Bartkowiak <patbar15@st.amu.edu.pl>
2020-04-02 13:40:42 +0200

    Limiting DataFrame based on user choice

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 import urllib.request, json
+import pandas as pd
 from tkinter import *
 
 
 def main():
+
+    app = Tk()
+    app.title("Coronavirus statistics")
 
     # Getting JSON data
     url = "https://opendata.ecdc.europa.eu/covid19/casedistribution/json/"
     response = urllib.request.urlopen(url)
     jsonData = json.loads(response.read())
 
+    # Labels for pandas DataFrame
+    labels = ["Date", "Cases", "Deaths", "Country"]
     data = []
     # Countries without duplicates
     COUNTRIES = []
@@ -19,9 +25,8 @@ def main():
         if row["countriesAndTerritories"] not in COUNTRIES:
             COUNTRIES.append(row["countriesAndTerritories"])
 
-
-    app = Tk()
-    app.title("Coronavirus statistics")
+    # Creating DataFrame based on data from JSON
+    df = pd.DataFrame(data, columns=labels)
 
     # Setting default value for country
     currentCountry = StringVar(app)
@@ -32,11 +37,17 @@ def main():
     currentTypeOfData = StringVar(app)
     currentTypeOfData.set("Cases")
 
+    # Displaying OptionMenu for country
     countryLabel = Label(app, text="Choose a country").grid(row=0, column=0)
     countryMenu = OptionMenu(app, currentCountry, *COUNTRIES).grid(row=0, column=1)
 
+    # Displaying OptionMenu for type of data
     typeOfDataLabel = Label(app, text="Choose type of data").grid(row=0, column=2)
     typeOfDataMenu = OptionMenu(app, currentTypeOfData, *TYPEOFDATA).grid(row=0, column=3)
+
+    # Getting values based on user options
+    result = df[["Date", currentTypeOfData.get()]].loc[df["Country"] == currentCountry.get()]
+    print(result.to_string(index=False))
 
     app.mainloop()
 
